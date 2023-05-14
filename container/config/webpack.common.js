36ac31a70625415a0d2d6d00413355dae549ce42
Masoud Banimahd <masoud@eliq.io>
2023-05-14 20:46:19 +0200

    remove app.css from app.tsx

--- container/config/webpack.common.js
+++ container/config/webpack.common.js
@@ -32,7 +32,7 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+    extensions: ['.*', '.js', '.jsx', '.ts', '.tsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({
--- container/src/App.tsx
+++ container/src/App.tsx
@@ -1,5 +1,3 @@
-import './App.css';
-
 import { MarketingModule } from './components/MarketingModule';
 
 function App() {
